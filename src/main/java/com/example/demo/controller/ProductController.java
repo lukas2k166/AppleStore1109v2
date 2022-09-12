916ca255bd51f1b3815b987cb91498946d426e38
lukaszmarczewski <lukas2k166@interia.pl>
2022-09-12 14:34:28 +0200

    feat: add permission based authentification

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,7 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
-    @PostMapping("/add/new/product")
+    @GetMapping("/add/new/product")
     public void addNewProduct(){
         productService.addNewProduct();
     }
---
+++ src/main/java/com/example/demo/security/ApplicationUserPermission.java
@@ -0,0 +1,18 @@
+package com.example.demo.security;
+
+public enum ApplicationUserPermission {
+
+    SEE_PRODUCT("see:product"),
+    ADD_PRODUCT("add:product");
+
+    private final String permission;
+
+    ApplicationUserPermission(String permission) {
+        this.permission = permission;
+    }
+
+    public String getPermission() {
+        return permission;
+    }
+
+}
