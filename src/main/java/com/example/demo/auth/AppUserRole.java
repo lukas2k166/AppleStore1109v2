72c9a32dcf26e742f9bf72a9dd7b64588ec2a0d2
lukaszmarczewski <lukas2k166@interia.pl>
2022-09-21 19:02:00 +0200

    refactor: remove old version of User Roles

--- src/main/java/com/example/demo/auth/AppUserRole.java
+++ src/main/java/com/example/demo/auth/AppUserRole.java
@@ -2,22 +2,16 @@ package com.example.demo.auth;
 
 import com.example.demo.security.config.ApplicationUserPermission;
 import com.google.common.collect.Sets;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import static com.example.demo.security.config.ApplicationUserPermission.ADD_PRODUCT;
 import static com.example.demo.security.config.ApplicationUserPermission.SEE_PRODUCT;
 
 public enum AppUserRole {
 
-    CLIENT
-            (Sets.newHashSet(SEE_PRODUCT))
-    ,
-    ADMIN
-            (Sets.newHashSet(SEE_PRODUCT,ADD_PRODUCT))
-            ;
+    CLIENT(Sets.newHashSet(SEE_PRODUCT)),
+    ADMIN(Sets.newHashSet(SEE_PRODUCT,ADD_PRODUCT));
 
     private final Set<ApplicationUserPermission> permissions;
 
@@ -29,12 +23,5 @@ public enum AppUserRole {
             return permissions;
         }
 
-        public Set<SimpleGrantedAuthority> getGrantedAuthorities() {
-            Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
-                    .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
-                    .collect(Collectors.toSet());
-            permissions.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
-            return permissions;
-        }
 
 }
