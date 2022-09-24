679b89f0ee3a7f964940f7a4ae55dcfad100f74b
lukaszmarczewski <lukas2k166@interia.pl>
2022-09-24 02:11:44 +0200

    refactor: add lombok annotations and remove boilerplate code part2

--- src/main/java/com/example/demo/testedPart/UserRegistrationDto.java
+++ src/main/java/com/example/demo/testedPart/UserRegistrationDto.java
@@ -1,5 +1,10 @@
 package com.example.demo.testedPart;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserRegistrationDto {
 
     private String username;
@@ -18,27 +23,4 @@ public class UserRegistrationDto {
         this.password = password;
     }
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRoles() {
-        return roles;
-    }
-
-    public void setRoles(String roles) {
-        this.roles = roles;
-    }
 }
