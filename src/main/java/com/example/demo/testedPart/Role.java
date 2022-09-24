e059840fdb38925669d9f4f678afed8ea7f3c807
lukaszmarczewski <lukas2k166@interia.pl>
2022-09-24 02:10:50 +0200

    refactor: add lombok annotations and remove boilerplate code

--- src/main/java/com/example/demo/testedPart/Role.java
+++ src/main/java/com/example/demo/testedPart/Role.java
@@ -1,5 +1,9 @@
 package com.example.demo.testedPart;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -7,6 +11,9 @@ import javax.persistence.Id;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "role")
 public class Role {
@@ -16,25 +23,8 @@ public class Role {
     private Long id;
     private String name;
 
-    public Role() {
-
-    }
-
     public Role(String name) {
         super();
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
         this.name = name;
     }
 }
