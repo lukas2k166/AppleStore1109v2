3ba7ef383a66d3e03856743099837e5313fabc2f
lukaszmarczewski <lukas2k166@interia.pl>
2022-09-27 01:51:19 +0200

    feat: add button that return all iphones

--- src/main/java/com/example/demo/controller/template_controllers/client_pages/product_pages/IphonesPageController.java
+++ src/main/java/com/example/demo/controller/template_controllers/client_pages/product_pages/IphonesPageController.java
@@ -1,13 +1,17 @@
 package com.example.demo.controller.template_controllers.client_pages.product_pages;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/homepage/products/")
+@RequestMapping("/homepage/products/iphones_client_page")
 public class IphonesPageController {
 
-
+    @GetMapping("")
+    public String getIphonesPage() {
+        return "iphones_client_page";
+    }
 
 
 }
