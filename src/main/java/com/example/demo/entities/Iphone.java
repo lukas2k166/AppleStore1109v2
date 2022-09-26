b1893ccd17664eb62c3264955a7cd43fde6de976
lukaszmarczewski <lukas2k166@interia.pl>
2022-09-27 01:13:24 +0200

    feat: add iphone entity class

---
+++ src/main/java/com/example/demo/entities/Iphone.java
@@ -0,0 +1,44 @@
+package com.example.demo.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Data
+@Table(name = "Iphones")
+@ToString(exclude = "product")
+public class Iphone {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "\"Iphone ID\"")
+    private Long iphone_id;
+
+    @Column(name = "\"Nazwa\"")
+    private String iphone_name;
+
+    @Column(name = "\"Cena\"")
+    private BigDecimal iphone_price;
+
+    @OneToOne(
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY,
+            optional = false
+    )
+    @JoinColumn(
+            name = "\"Iphone ID\"",
+            referencedColumnName = "\"Iphone ID\""
+    )
+    private Product product;
+
+
+
+
+
+
+}
