2082a8a32a51746641f672744985066f196d128e
lukaszmarczewski <lukas2k166@interia.pl>
2022-10-02 00:34:26 +0200

    fix: block non admin users to access deleteMacbook method typing url in browser(without using buttons)

--- src/main/java/com/example/demo/controller/rest_controllers/MacbookController.java
+++ src/main/java/com/example/demo/controller/rest_controllers/MacbookController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller.rest_controllers;
 import com.example.demo.entities.Macbook;
 import com.example.demo.service.MacbookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -37,6 +38,7 @@ public class MacbookController {
 
     @RequestMapping(value = "/delete",
             method={RequestMethod.DELETE, RequestMethod.GET})
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     public void deleteMacbook() {
         macbookService.deleteMacbook();
     }
