4f9de7e130603df1a939a13c27db858961ed3642
lukaszmarczewski <lukas2k166@interia.pl>
2022-10-02 00:54:00 +0200

    fix: block non admin users to access deleteMacbook method typing url in browser(without using buttons) did not work[solved]

--- src/main/java/com/example/demo/controller/rest_controllers/IphoneController.java
+++ src/main/java/com/example/demo/controller/rest_controllers/IphoneController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller.rest_controllers;
 import com.example.demo.entities.Iphone;
 import com.example.demo.service.IphoneService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,6 +18,7 @@ import static com.example.demo.authorization.AppUserRole.ADMIN;
 @Configuration
 @AllArgsConstructor
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserServiceImpl userServiceimpl;
