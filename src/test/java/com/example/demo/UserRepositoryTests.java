c3f8705ea236775579ec22a80bc36d67224c06d1
lukaszmarczewski <lukas2k166@interia.pl>
2022-10-04 04:46:45 +0200

    test: find user by username

--- src/test/java/com/example/demo/UserRepositoryTests.java
+++ src/test/java/com/example/demo/UserRepositoryTests.java
@@ -5,7 +5,6 @@ import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -26,25 +25,33 @@ class UserRepositoryTests {
 	@Autowired
 	private UserRepository underTest;
 
-	private User newUser;
-
-	@AfterEach
-	void deleteCreatedUser(){
-		underTest.deleteByUsername(newUser.getUsername());
-	}
-
 	@Test
 	void shouldRegisterUser(){
 
-				newUser = entityManager.persist(new User(
-						"TestUser","password",
+				User newUser = entityManager.persist(new User(
+						"TestUser44","password",
 				new LinkedList<>(List.of(new Role("ROLE_ADMIN"))
 				)));
 
 		underTest.save(newUser);
 
+
 		assertThat(newUser).isNotNull();
+
+		underTest.deleteById(newUser.getId());
 	}
+
+	@Test
+	public void shouldFindUserByUsername() {
+		User user = new User();
+		user.setUsername("AdminUser");
+
+		assertThat(underTest.findByUsername(user.getUsername()).get().getUsername())
+				.isEqualTo(user.getUsername());
+	}
+
+
+
 
 
 }
