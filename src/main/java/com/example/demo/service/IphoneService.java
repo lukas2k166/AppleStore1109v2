c27de858c73fce6537b45d3b52be350f49b79008
lukaszmarczewski <lukas2k166@interia.pl>
2022-10-08 12:12:18 +0200

    fix: correct mapping from FindSpecificIphone button

--- src/main/java/com/example/demo/service/IphoneService.java
+++ src/main/java/com/example/demo/service/IphoneService.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
-import java.util.Scanner;
 
 import static com.example.demo.service.Categories.PHONE;
 
